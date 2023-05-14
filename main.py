f30c97525fcd36c2c34dccef35aac427c25480bc
skyline8880 <skyline89_88@mail.ru>
2023-05-14 20:17:13 +0300

    ver002

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 from kivy.animation import Animation
 from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.toolbar import MDTopAppBar
@@ -20,29 +21,41 @@ def resource_path(relative_path):
 custom_img = resource_path('debt.png')
 custom_icon = resource_path('debt_ico.ico')
 
+
 class DebtCounterApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def change_type(self):
         if self.state == 0:
             self.state = 1
-            self.toolbar.title = ("қарзи аннуитетӣ").upper()
+            self.toolbar.title = self.appLanguage[self.lang]["annuity"]
         else:
             self.state = 0
-            self.toolbar.title = ("қарзи дифференсиалӣ").upper()
+            self.toolbar.title = self.appLanguage[self.lang]["differentiated"]
 
     def calculate(self, args=None, kwargs=None):
-        if (self.loan_input.text == '' or self.terms_input.text == '' or self.interest_input.text == ''):
+        if (self.loan_input.text == "" or self.terms_input.text == "" or self.interest_input.text == ""):
             self.outro_anim(self.payment_per_month)
             self.outro_anim2(self.interest_a_day)
             self.outro_anim3(self.interest_hole_period)
             self.outro_anim4(self.payment_hole_period)
 
         try:
+            for input in [self.loan_input, self.terms_input]:
+                if input.text.startswith("0"):
+                    input.text = ""
+            if int(self.interest_input.text) < 1:
+                self.interest_input.text = "1"
+            elif int(self.interest_input.text) > 100:
+                self.interest_input.text = "100"
+
             loan = int(self.loan_input.text)
             terms = int(self.terms_input.text)
             interest = float(self.interest_input.text)
             today = dt.datetime.today()
             num_of_days_in_month = int(calendar.monthrange(today.year, today.month)[1])
-            if self.toolbar.title == ("қарзи аннуитетӣ").upper():
+            if self.state == 1:
                 interest_per_month = interest / (100*12)
                 annuity_payment = round(float(
                     loan * ((interest_per_month *
@@ -130,54 +143,135 @@ class DebtCounterApp(MDApp):
         anim = Animation(pos_hint={'center_x': -1  }, duration=.50)
         anim.start(widget)
 
+    def clean_panel(self, *args):
+        self.loan_input.text = ''
+        self.terms_input.text = ''
+        self.interest_input.text = ''
+
+    def menu_open(self, instance):
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": i,
+                "on_release": lambda x=i: self.menu_callback(x),
+            } for i in self.list_items
+        ]
+
+        self.menu = MDDropdownMenu(
+            caller=instance, 
+            items=menu_items,
+            width_mult=2,
+        )
+        self.menu.open()
+
+    def menu_callback(self, text_item):
+        self.lang = text_item
+        self.title = self.appLanguage[self.lang]["appname"]
+        if self.state == 1:
+            self.toolbar.title = self.appLanguage[self.lang]["annuity"]
+        else:
+            self.toolbar.title = self.appLanguage[self.lang]["differentiated"]
+        self.loan_input.hint_text = self.appLanguage[self.lang]["loanvalue"]
+        self.terms_input.hint_text = self.appLanguage[self.lang]["terms"]
+        self.interest_input.hint_text = self.appLanguage[self.lang]["interest"]
+        self.payment_per_month.hint_text = self.appLanguage[self.lang]["monthlypayment"]
+        self.interest_a_day.hint_text = self.appLanguage[self.lang]["interestday"]
+        self.interest_hole_period.hint_text = self.appLanguage[self.lang]["interestperiod"]
+        self.payment_hole_period.hint_text = self.appLanguage[self.lang]["paymentperiod"]
+        self.menu.dismiss()
+
     def build(self):
-        self.title = 'Ҳисобкунаки пардохт'
+        self.lang = "English"
+        self.appLanguage = {
+            "English": {
+                "appname": "Payment calculator",
+                "annuity": "Annuity loan",
+                "differentiated": "Differentiated loan",
+                "loanvalue": "Loan value",
+                "terms": "Terms",
+                "interest": "Interest",
+                "monthlypayment": "Monthly payment",
+                "interestday": "Interest per day",
+                "interestperiod": "Interest at all period",
+                "paymentperiod": "Payment at all period"
+            },
+            "Русский": {
+                "appname": "Калькулятор платежей",
+                "annuity": "Аннуитетный кредит",
+                "differentiated": "Дифференцированный кредит",
+                "loanvalue": "Сумма кредита",
+                "terms": "Срок кредита",
+                "interest": "Процентаная ставка",
+                "monthlypayment": "Ежемесячный платёж",
+                "interestday": "Сумма процента в день",
+                "interestperiod": "Сумма процентов за весь период",
+                "paymentperiod": "Сумма платежей за весь период"
+            },
+            "Тоҷикӣ": {
+                "appname": "Ҳисобкунаки пардохт",
+                "annuity": "Қарзи аннуитетӣ",
+                "differentiated": "Қарзи дифференсиалӣ",
+                "loanvalue": "Маблағи қарз",
+                "terms": "Муҳлати қарз",
+                "interest": "Фоизи солонаи қарз",
+                "monthlypayment": "Пардохти моҳона",
+                "interestday": "Пардохти фоизҳо дар як руз",
+                "interestperiod": "Пардохти фоизҳо барои тамоми муҳлат",
+                "paymentperiod": "Пардохтҳои пурраи муҳлат"
+            }
+        }
+
+
+        self.title = self.appLanguage[self.lang]["appname"]
         self.icon = custom_img
         self.state = 1
         self.theme_cls.primary_palette = "Cyan"
         self.screen = MDScreen()
         self.toolbar = MDTopAppBar(
-            title=("қарзи аннуитетӣ").upper(),
+            title=self.appLanguage[self.lang]["annuity"],
             md_bg_color=(0, 20, 21.2, 0.8),
             opposite_colors=True
             )
         self.toolbar.pos_hint = {"top": .999}
         self.toolbar.right_action_items = [
-            ["rotate-3d-variant", lambda x: (self.change_type(), self.calculate())]]
+            ["rotate-3d-variant", lambda x: (self.change_type(), self.calculate())],
+            ["recycle", lambda x: self.clean_panel()],
+            ["earth", lambda x: self.menu_open(x)]]
         
         self.screen.add_widget(self.toolbar)
 
+        self.list_items = [
+            "English", "Русский", "Тоҷикӣ"
+        ]
+
         self.loan_input = MDTextField(
-            hint_text="маблағи қарз",
+            hint_text=self.appLanguage[self.lang]["loanvalue"],
             input_filter="int",
             write_tab=False,
             multiline=False,
-            halign="right",
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": 0.5, "center_y":0.82},
-            font_size=18,
         )
         self.loan_input.bind(text=self.calculate)
         self.terms_input = MDTextField(
-            hint_text="мӯҳлати қарз",
+            hint_text=self.appLanguage[self.lang]["terms"],
             input_filter="int",
             write_tab=False,
             multiline=False,
-            halign="right",
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": 0.5, "center_y":0.75},
-            font_size=18,
         )
         self.terms_input.bind(text=self.calculate)
         self.interest_input = MDTextField(
-            hint_text="фоизи солонаи қарз",
+            hint_text=self.appLanguage[self.lang]["interest"],
             input_filter="float",
             write_tab=False,
             multiline=False,
-            halign="right",
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": 0.5, "center_y":0.68},
-            font_size=18,
         )
         self.interest_input.bind(text=self.calculate)
 
@@ -187,35 +281,31 @@ class DebtCounterApp(MDApp):
 
         self.payment_per_month = MDTextField(
             readonly=True,
-            hint_text="пардохти моҳона",
-            halign="right",
+            hint_text=self.appLanguage[self.lang]["monthlypayment"],
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": -1, "center_y": 0.56},
-            font_size=18
         )
         self.interest_a_day = MDTextField(
             readonly=True,
-            hint_text="пардохти фоизҳо дар як руз",
-            halign="right",
+            hint_text=self.appLanguage[self.lang]["interestday"],
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": -1, "center_y": 0.49},
-            font_size=18
         )
         self.interest_hole_period = MDTextField(
             readonly=True,
-            hint_text="пардохти фоизҳо барои тамоми муҳлат",
-            halign="right",
+            hint_text=self.appLanguage[self.lang]["interestperiod"],
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": -1, "center_y": 0.42},
-            font_size=18
         )
         self.payment_hole_period = MDTextField(
             readonly=True,
-            hint_text="пардохтҳои пурраи муҳлат",
-            halign="right",
+            hint_text=self.appLanguage[self.lang]["paymentperiod"],
+            halign="left",
             size_hint=(0.3, 1),
             pos_hint={"center_x": -1, "center_y": 0.35},
-            font_size=18
         )
 
         self.screen.add_widget(self.payment_per_month)
